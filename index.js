c303aa09b76664a2e50a7e438294a8809c4aba5e
Bakul Mukherjee <98204727+bakulm4@users.noreply.github.com>
2022-06-02 10:53:41 -0700

    Add UI for game

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll('button');
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+const resultElement = document.querySelector('#result');
+
 function computerPlay(){
     const choices=['Rock','Paper','Scissors'];
     return choices[Math.floor(Math.random()*3)];
@@ -45,4 +50,37 @@ function game(){
     console.log( count<0 ? 'Sorry you lose!': count === 0 ? `It's a tie`:'Yay! you won!');
 }
 
-game();
+//game();
+let userScore = 0;
+let computerScore = 0;
+let gameOver = false;
+function handleclick(e){
+    //console.log(this.textContent);
+    if(gameOver){
+        resultElement.textContent = 'Winner:';
+        //score.textContent = '';
+        userScore = 0;
+        computerScore = 0;
+        gameOver = false;
+    }
+   
+    //console.log(playRound(userSelection,computerPlay()));
+    const userSelection = this.textContent;
+    const result = playRound(userSelection,computerPlay());
+    if(/^You Lose!/.test(result))
+        computerScore += 1;
+    else if(/^You Win!/.test(result))
+        userScore +=1;
+
+    score.textContent = `Score: You : ${userScore} , Computer :  ${computerScore}`;
+    
+    if(userScore === 5 || computerScore === 5){
+        resultElement.textContent = userScore<computerScore ? 'Sorry you lose!': userScore === computerScore  ? `It's a tie`:'Yay! you won!';
+        results.appendChild(resultElement);
+        gameOver = true;
+    }
+}
+
+buttons.forEach(button=>{
+    button.addEventListener('click', handleclick);
+})
